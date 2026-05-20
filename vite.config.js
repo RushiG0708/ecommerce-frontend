96a1754b5498fbeee99e9fc89db56b2639e463aa
Rushikesh <rushikeshgade8080@gmail.com>
2026-05-20 11:06:50 +0530

    perf: fix code splitting for Vite 8

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            if (id.includes("react-dom") || id.includes("react-router")) {
+              return "vendor";
+            }
+            if (id.includes("@reduxjs") || id.includes("react-redux")) {
+              return "redux";
+            }
+            if (id.includes("recharts")) {
+              return "charts";
+            }
+            if (id.includes("react-icons")) {
+              return "icons";
+            }
+            return "libs";
+          }
+        },
+      },
+    },
+  },
+});
